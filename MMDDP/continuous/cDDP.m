ecfc7f216b3dc576c045254b6fe97ea991264d24
Zhefeng <caozhefeng1998@gmail.com>
2019-10-24 16:55:55 -0400

    cts version

--- MMDDP/continuous/cDDP.m
+++ MMDDP/continuous/cDDP.m
@@ -13,17 +13,19 @@ function cDDP
 % cost parameter
 Q=diag([1 0.1]);
 Ru=0.01;
+Rv=1;
 
 % time horizon N
-N=100;
+N=1000;
 % dimenison
 n = 2;  % state
 m = 1;  % control
 % interval
-dt=0.01;
+dt=0.001;
 
 % initial
 u=zeros(m,N);
+v=zeros(m,N);
 x=zeros(n,N);x(:,1)=[pi,0]';
 dx=zeros(n,N);dx(:,1)=[0;0];
 gamma=0.8;
@@ -33,7 +35,7 @@ itr=0;
 fprintf('\n=========== begin Min-Max DDP ===========\n');
 while(1)
     %%%% initial trajectory %%%%%
-    [x, fx, fu]=dynamics(x,u,dt);
+    [x, fx, fu, fv]=dynamics(x,u,v,dt);
     % tspan = [0 1];
     % x0=[pi;0];
     % [xt,x] = ode45(@(t,x) dynamics(t,x,ut,u), tspan, x0);
@@ -43,33 +45,42 @@ while(1)
     % end
 
     % calculate value function at final time
+%     V_x(:,N)=2*Q*x(:,N);
+%     V_xx(:,:,N)=2*Q;
     V_x(:,N)=2*Q*x(:,N);
     V_xx(:,:,N)=2*Q;
 
     for i=N-1:-1:1
         % Q_x\ Q_xx change here
-        V_x(:,i)=V_x(:,i+1)+dt*(2*Q*x(:,i+1)+fx(:,:,i+1)'*V_x(:,i+1)-V_xx(:,:,i+1)'*fu(:,:,i+1)*(2*Ru*u(:,i)+fu(:,:,i+1)'*V_x(:,i+1))/(2*Ru));
-        V_xx(:,:,i)=V_xx(:,:,i+1)+dt*(2*Q+V_xx(:,:,i+1)*fx(:,:,i+1)+fx(:,:,i+1)'*V_xx(:,:,i+1)-V_xx(:,:,i+1)'*fu(:,:,i+1)*fu(:,:,i+1)'*V_xx(:,:,i+1)/(2*Ru));
+        V_x(:,i)=V_x(:,i+1)+dt*(2*Q*x(:,i+1)+fx(:,:,i+1)'*V_x(:,i+1)-V_xx(:,:,i+1)'*fu(:,:,i+1)*(2*Ru*u(:,i)+fu(:,:,i+1)'*V_x(:,i+1))/(2*Ru)-V_xx(:,:,i+1)'*fv(:,:,i+1)*(2*Rv*v(:,i)+fv(:,:,i+1)'*V_x(:,i+1))/(2*Rv));
+        V_xx(:,:,i)=V_xx(:,:,i+1)+dt*(2*Q+V_xx(:,:,i+1)*fx(:,:,i+1)+fx(:,:,i+1)'*V_xx(:,:,i+1)-V_xx(:,:,i+1)'*fu(:,:,i+1)*fu(:,:,i+1)'*V_xx(:,:,i+1)/(2*Ru)-V_xx(:,:,i+1)'*fv(:,:,i+1)*fv(:,:,i+1)'*V_xx(:,:,i+1)/(2*Rv));
 
         Q_u(:,i)=2*u(:,i)*Ru+fu(:,:,i)'*V_x(:,i);
         Q_uu(:,:,i)=2*eye(m)*Ru;
         Q_ux(:,:,i)=fu(:,:,i)'*V_xx(:,:,i+1);
+        Q_v(:,i)=-2*v(:,i)*Rv+fv(:,:,i)'*V_x(:,i);
+        Q_vv(:,:,i)=-2*eye(m)*Rv;
+        Q_vx(:,:,i)=fv(:,:,i)'*V_xx(:,:,i+1);
         lu(:,i)=-Q_u(:,i)/Q_uu(:,:,i);
-        Ku(:,:,i)=-Q_ux(:,:,i)/Q_uu(:,:,i); 
+        Ku(:,:,i)=-Q_ux(:,:,i)/Q_uu(:,:,i);
+        lv(:,i)=-Q_v(:,i)/Q_vv(:,:,i);
+        Kv(:,:,i)=-Q_vx(:,:,i)/Q_vv(:,:,i); 
     end
 
     for i=1:N-1
-        dx(:,i+1)=dx(:,i)+dt*(fu(:,:,i)*lu(:,i)+(fx(:,:,i)+fu(:,:,i)*Ku(:,:,i))*dx(:,i));
+        dx(:,i+1)=dx(:,i)+dt*(fu(:,:,i)*lu(:,i)+fv(:,:,i)*lv(:,i)+(fx(:,:,i)+fu(:,:,i)*Ku(:,:,i)+fv(:,:,i)*Kv(:,:,i))*dx(:,i));
         du(:,i)=lu(:,i)+Ku(:,:,i)*dx(:,i);
+        dv(:,i)=lv(:,i)+Kv(:,:,i)*dx(:,i);
     end
     du(:,N)=0;
+    dv(:,N)=0;
     u=u+du*gamma;
+    v=v+dv*gamma;
 
     itr=itr+1;
 %     cost(itr)=sum(u.*u*Ru)*dt+x(:,N)'*Q*x(:,N);
 
-    max(abs(du))
-    if max(abs(du))< 1e-5
+    if max(abs(du))+ max(abs(dv))< 1e-5
     %     if itr>=20
         break;
     end
@@ -86,24 +97,16 @@ fprintf(['\n'...
     '=========== end BSDDP ===========\n']);
 
 % %% Plot
-% 
-% % control sequence
-% figure(1);
-% hold on;
-% % for i=1:N
-% %   mean_true(i)=sum(x_true(i,:))/SAM;
-% %   cov_true(i)=sum((x_true(i,:)-mean_true(i)*ones(1,SAM)).^2)/SAM;
-% % end
-% plot(0:dt:dt*(N-2),v(1,:),'b','linewidth',2);
-% plot(0:dt:dt*(N-2),u(1,:),'g','linewidth',2);
-% 
-% title('Control sequence');
-% xlabel('Time in sec');
-% hold off;
-% 
-% % Cost
-% figure(2);
-% plot(1:itr,cost,'r','linewidth',2);
-% title('Control sequence');
-% xlabel('Iteration');
-% end
+% control sequence
+figure(1);
+plot(0:dt:dt*(N-1),u(1,:),0:dt:dt*(N-1),v(1,:),'linewidth',2);
+title('Control sequence');
+xlabel('Time in sec');
+ylabel('u');
+
+% state trajectory
+figure(3)
+plot(0:dt:dt*(N-1),x(1,:),0:dt:dt*(N-1),x(2,:),'linewidth',2);
+title('state trajectory');
+
+end
--- MMDDP/continuous/dynamics.m
+++ MMDDP/continuous/dynamics.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Dynamics
 % theta=theta+dt*(theta_dot)
-% theta_dot=theta_dot+dt*(sin(theta)-theta_dot+u)
+% theta_dot=theta_dot+dt*(sin(theta)-theta_dot+u+v)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [xnew, fx, fu]=dynamics(x,u,dt)
+function [xnew, fx, fu, fv]=dynamics(x,u,v,dt)
 N=size(x,2);
 n=size(x,1);
 
@@ -13,12 +13,14 @@ xnew(:,1)=x(:,1);
 fx(:,:,1)=[0, 1;...
         9.81*2*cos(xnew(1,1)), -0.4];
 fu(:,:,1)=[0;4];
+fv(:,:,1)=[0;4];
 
 for i=1:N-1
-    xnew(:,i+1)=xnew(:,i)+dt*[xnew(2,i); 9.81*2*sin(xnew(1,i))-0.4*xnew(2,i)+4*u(1,i)];
+    xnew(:,i+1)=xnew(:,i)+dt*[xnew(2,i); 9.81*2*sin(xnew(1,i))-0.4*xnew(2,i)+4*u(1,i)+4*v(1,i)];
     fx(:,:,i+1)=[0, 1;...
         9.81*2*cos(xnew(1,i+1)), -0.4];
     fu(:,:,i+1)=[0;4];
+    fv(:,:,i+1)=[0;4];
 end
 
 end
