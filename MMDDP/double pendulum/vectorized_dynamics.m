133e236c0d79aea7b61c6f6d8c1a74aef8c66c03
iscoyizj <36182198+iscoyizj@users.noreply.github.com>
2019-11-22 03:31:23 +0800

    do dynamics with vectorized

---
+++ MMDDP/double pendulum/vectorized_dynamics.m
@@ -0,0 +1,21 @@
+% vectorized dynamics
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Dynamics
+% theta=theta+dt*(theta_dot)
+% theta_dot=theta_dot+dt*(sin(theta)-theta_dot+u+v)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% dxdt is n*d matrix n is the number of data d is the dimension of state
+% u v is both n*k k is the dimension of control
+
+function dxdt=dynamics(x,u,v)
+
+m=1;
+l=0.5;
+g=9.81;
+
+dxdt(:,1)=x(:,3);
+dxdt(:,2)=x(:,4);
+dxdt(:,3)=(((u(:,1)+v(:,1))-(u(:,2)+v(:,2)).*cos(x(:,1)-x(:,2)))./(m*l^2)-sin(x(:,1)-x(:,2)).*(x(:,3).^2.*cos(x(:,1)-x(:,2))+x(:,4).^2)+g/l*(2*sin(x(:,1))-sin(x(:,2)).*cos(x(:,1)-x(:,2))))./(2-cos(x(:,1)-x(:,2)).^2);
+dxdt(:,4)=((2*(u(:,2)+v(:,2))-(u(:,1)+v(:,1)).*cos(x(:,1)-x(:,2)))./(m*l^2)+sin(x(:,1)-x(:,2)).*(2*x(3).^2+x(4).^2.*cos(x(:,1)-x(:,2)))+2*g/l*(sin(x(:,2))-sin(x(:,1)).*cos(x(:,1)-x(:,2))))./(2-cos(x(:,1)-x(:,2)).^2);
+end
